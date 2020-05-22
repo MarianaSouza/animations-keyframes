3bcc4eb2736a79785933405197a48197154007c4
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-05-21 21:16:25 -0700

    adding function to handle animations when scrolling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
-//This function allows to aplly a scroll effect to any "a" that has an "id" as a target (internal link)
-$(document).ready(function(){
-    $('a[href^="#"].nav-link').on('click',function (e) {
-    e.preventDefault();
-    var target = this.hash;
-    $target = $(target);
-        $('html, body').stop().animate({'scrollTop': $target.offset().top}, 1500, 'swing', function () {
-        window.location.hash = target;
+$(document).ready(function () {
+    //This function allows to aplly a scroll effect to any "a" that has an "id" as a target (internal link)
+    $('a[href^="#"].nav-link').on('click', function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({ 'scrollTop': $target.offset().top }, 1500, 'swing', function () {
+            window.location.hash = target;
         });
+    });
+     
+    //This function activates the animations when scrolling
+    $(window).trigger('scroll');
+    var $elementRight= $('.animate-element-right');
+    var $elementLeft= $('.animate-element-left');
+    var $elementCubic = $('.animate-element-cubic');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $elementCubic.addClass("animate-properties fadeInUpCubic");
+            $elementRight.addClass("animate-properties fadeInRight");
+            $elementLeft.addClass("animate-properties fadeInLeft");
+        }
+        else {
+            $elementCubic.removeClass("animate-properties fadeInUpCubic");
+            $elementRight.removeClass("animate-properties fadeInRight");
+            $elementLeft.removeClass("animate-properties fadeInLeft");
+        }
     });
 });
